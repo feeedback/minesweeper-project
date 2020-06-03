a5c07b4a05ea3d8f23194220508530d6d31ff33e
A. Rzhanitsyn <risdeep@gmail.com>
2020-06-03 23:45:16 +0500

    Updates

---
+++ precache-manifest.ca74883b8b12df11dd88ba1f7cdda8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce624485ed0a1d94a0f025fa9d94b76b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "313c0a8d81826336e272",
+    "url": "./static/css/main.0c8ecaa1.chunk.css"
+  },
+  {
+    "revision": "3951e9e973877949505a",
+    "url": "./static/js/2.d45a7a1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d45a7a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313c0a8d81826336e272",
+    "url": "./static/js/main.f387535c.chunk.js"
+  },
+  {
+    "revision": "462618f21333fda5a5b9",
+    "url": "./static/js/runtime-main.71d5a5ac.js"
+  }
+]);
