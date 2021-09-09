ab43b9e7401ef4d9c2357e5c1ce05a6d92715fc5
A. Rzhanitsyn <risdeep@gmail.com>
2021-09-09 19:51:49 +0500

    eslint fix

--- src/components/BoardGame.jsx
+++ src/components/BoardGame.jsx
@@ -2,76 +2,76 @@ import React from 'react';
 import './BoardGame.css';
 
 const mapCellValueToText = {
-    0: '',
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
-    8: '8',
+  0: '',
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '8',
 
-    M: '💣', // mine
-    ME: '💣', // mine explosion
-    C: '', // '?',  closed cell
-    F: '🚩', //  🏴⚑🚩flagged cell
-    FW: '✕', // ✕✖❌wrong flagged cell
+  M: '💣', // mine
+  ME: '💣', // mine explosion
+  C: '', // '?',  closed cell
+  F: '🚩', //  🏴⚑🚩flagged cell
+  FW: '✕', // ✕✖❌wrong flagged cell
 };
 const mapCellValueToClassName = {
-    0: 'empty',
-    1: 'digit1',
-    2: 'digit2',
-    3: 'digit3',
-    4: 'digit4',
-    5: 'digit5',
-    6: 'digit6',
-    7: 'digit7',
-    8: 'digit8',
+  0: 'empty',
+  1: 'digit1',
+  2: 'digit2',
+  3: 'digit3',
+  4: 'digit4',
+  5: 'digit5',
+  6: 'digit6',
+  7: 'digit7',
+  8: 'digit8',
 
-    M: 'mine',
-    ME: 'mine mineExplosion',
-    C: 'closed',
-    F: 'closed flagged',
-    FW: 'flaggedWrong',
+  M: 'mine',
+  ME: 'mine mineExplosion',
+  C: 'closed',
+  F: 'closed flagged',
+  FW: 'flaggedWrong',
 };
 const mapCellValueToLabel = {
-    0: 'empty',
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
-    8: '8',
+  0: 'empty',
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '8',
 
-    M: 'mine',
-    ME: 'explosion mine',
-    C: 'closed',
-    F: 'flagged',
-    FW: 'wrong flagged',
+  M: 'mine',
+  ME: 'explosion mine',
+  C: 'closed',
+  F: 'flagged',
+  FW: 'wrong flagged',
 };
 
 const BoardGame = ({ closedField, fieldIsDisable, boardHandles }) => {
-    return (
-        <div className="fieldContainer" {...boardHandles}>
-            {closedField.map((row, y) =>
-                row.map((cell, x) => (
-                    <button
-                        key={`Cell__${x}_${y}`}
-                        data-x={x}
-                        data-y={y}
-                        className={`Cell ${mapCellValueToClassName[cell]}`}
-                        disabled={fieldIsDisable[y][x]}
-                        aria-label={mapCellValueToLabel[cell]}
-                    >
-                        {mapCellValueToText[cell]}
-                    </button>
-                ))
-            )}
-        </div>
-    );
+  return (
+    <div className="fieldContainer" {...boardHandles}>
+      {closedField.map((row, y) =>
+        row.map((cell, x) => (
+          <button
+            key={`Cell__${x}_${y}`}
+            data-x={x}
+            data-y={y}
+            className={`Cell ${mapCellValueToClassName[cell]}`}
+            disabled={fieldIsDisable[y][x]}
+            aria-label={mapCellValueToLabel[cell]}
+          >
+            {mapCellValueToText[cell]}
+          </button>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default BoardGame;
